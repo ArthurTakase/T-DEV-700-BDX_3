5e041e0afc96bb2d2518dd46e05decba434dbc39
Arthur <arthur.decaen@epitech.eu>
2023-11-20 16:05:21 +0100

    FEAT: products UI

--- mobile/app/src/main/java/com/example/test/MainActivity.kt
+++ mobile/app/src/main/java/com/example/test/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.test.ui.theme.TestTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
+
+        val products = arrayOf(
+            Product("Arthur", 10, "Le plus beau", R.drawable.arthur),
+            Product("Corentin", 100, "Presque le plus beau", R.drawable.corentin),
+            Product("Theau", 30, "Occasion, très peu utilisé", R.drawable.theau),
+            Product("Guillaume", 50, "Chat GPT", R.drawable.guillaume),
+            Product("Geoffrey", 42, "Une grosse merde", R.drawable.geoffrey)
+        )
+
+        for (product in products) {
+            val productItem = LayoutInflater.from(this).inflate(R.layout.product_card, mainLayout, false)
+            productItem.findViewById<TextView>(R.id.product_name).text = product.name
+            productItem.findViewById<TextView>(R.id.product_price).text = "$${product.price} - ADD TO CART"
+            productItem.findViewById<TextView>(R.id.product_description).text = product.description
+            productItem.findViewById<ImageView>(R.id.product_image).setImageResource(product.image)
+
+            mainLayout.addView(productItem)
+        }
     }
 }
---
+++ mobile/app/src/main/java/com/example/test/Product.kt
@@ -0,0 +1,8 @@
+package com.example.test
+
+class Product(name: String?, price: Int, description: String?, image: Int) {
+    var name: String? = name
+    var price = price
+    var description: String? = description
+    var image = image
+}

93b6c0b294330885c2878f48146b66e23c42fa62
Arthur <arthur.decaen@epitech.eu>
2023-11-21 13:57:09 +0100

    FEAT: navbar

--- mobile/app/src/main/java/com/example/test/MainActivity.kt
+++ mobile/app/src/main/java/com/example/test/MainActivity.kt
@@ -8,14 +8,32 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.test.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Store())
 
-        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.store -> { replaceFragment(Store()); true }
+                R.id.cart -> { replaceFragment(Cart()); true }
+                R.id.settings -> { replaceFragment(Settings()); true }
+                else -> false
+
+            }
+        }
+
+        /*val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
 
         val products = arrayOf(
             Product("Arthur", 10, "Le plus beau", R.drawable.arthur),
@@ -33,6 +51,13 @@ class MainActivity : ComponentActivity() {
             productItem.findViewById<ImageView>(R.id.product_image).setImageResource(product.image)
 
             mainLayout.addView(productItem)
-        }
+        }*/
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }

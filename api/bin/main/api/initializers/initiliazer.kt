2363882fa8334a2c9029368215b8b67076a96a2b
Geoffrey <geoffrey.labruyere@epitech.eu>
2023-11-28 15:59:33 +0100

    [FIX] db connection wrong name for user postrgres -> postgres

--- api/bin/main/api/initializers/initiliazer.kt
+++ api/bin/main/api/initializers/initiliazer.kt
@@ -1,5 +1,5 @@
 class Initializer() {
-    public var database = Database("jdbc:postgresql://db:5432/dev_pg_db_cm?user=postrgres&password=postgres")
+    public var database = Database("jdbc:postgresql://db:5432/dev_pg_db_cm?user=postgres&password=postgres")
 
     init {
         val migrationUsers = MigrationUsers(database)

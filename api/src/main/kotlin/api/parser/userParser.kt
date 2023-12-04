6416f3472f73e3089cc07f932948fbae78d91b2d
roy-corentin <corentin.roy@epitech.eu>
2023-12-04 16:44:49 +0100

    [UP] lint files

--- api/src/main/kotlin/api/parser/userParser.kt
+++ api/src/main/kotlin/api/parser/userParser.kt
@@ -1,15 +1,12 @@
-
-
 import java.time.OffsetDateTime
 
-class UserParser(val id: Int, val name: String, val created_at: OffsetDateTime, val updated_at: OffsetDateTime)
-{
+class UserParser(
+    val id: Int,
+    val name: String,
+    val created_at: OffsetDateTime,
+    val updated_at: OffsetDateTime,
+) {
     fun parse(): User {
-        return User(
-            id = id,
-            name = name,
-            created_at = created_at,
-            updated_at = updated_at
-        )
+        return User(id = id, name = name, created_at = created_at, updated_at = updated_at)
     }
 }
--- api/src/main/kotlin/api/repositories/UserRepository.kt
+++ api/src/main/kotlin/api/repositories/UserRepository.kt
@@ -1,5 +1,3 @@
-
-import com.github.jasync.sql.db.general.ArrayRowData
 import java.time.OffsetDateTime
 
 class UserRepository(database: Database) {
@@ -9,14 +7,15 @@ class UserRepository(database: Database) {
 
     fun all() {
         val result = database.execQuery("select * from USERS").get()
-        val users = result.rows.map { row ->
-             UserParser(
-                id = row.get("id") as Int,
-                name = row.get("name") as String,
-                created_at = row.get("created_at") as OffsetDateTime,
-                updated_at = row.get("updated_at") as OffsetDateTime
-            ).parse()
-        }
+        val users =
+            result.rows.map { row ->
+                UserParser(
+                    id = row.get("id") as Int,
+                    name = row.get("name") as String,
+                    created_at = row.get("created_at") as OffsetDateTime,
+                    updated_at = row.get("updated_at") as OffsetDateTime,
+                ).parse()
+            }
         println(users)
     }
 

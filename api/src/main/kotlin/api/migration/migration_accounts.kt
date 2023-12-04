cd008f2d4f69dcc23446270b35cb208151476268
roy-corentin <corentin.roy@epitech.eu>
2023-12-04 09:30:11 +0100

    [UP] lint

--- api/src/main/kotlin/api/migration/migration_accounts.kt
+++ api/src/main/kotlin/api/migration/migration_accounts.kt
@@ -1,12 +1,10 @@
-
-
-class MigrationAccounts(database: Database)
-{
+class MigrationAccounts(database: Database) {
     private val database = database
 
-    fun exec()
-    {
-        database.execQuery("CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY, account_number INTEGER, sold INTEGER);")
+    fun exec() {
+        database.execQuery(
+            "CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY, account_number INTEGER, sold INTEGER);",
+        )
         print("Migration accounts")
     }
 }
--- api/src/main/kotlin/api/migration/migration_users.kt
+++ api/src/main/kotlin/api/migration/migration_users.kt
@@ -1,11 +1,7 @@
-
-
-class MigrationUsers(database: Database)
-{
+class MigrationUsers(database: Database) {
     private val database = database
 
-    fun exec()
-    {
+    fun exec() {
         database.execQuery("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255));")
         print("Migration users")
     }

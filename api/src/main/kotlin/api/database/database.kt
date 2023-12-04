6bec675d6b9186dae19c296a4a46779b9ee1ac6c
roy-corentin <corentin.roy@epitech.eu>
2023-12-04 11:52:05 +0100

    [UP] execQuery return the future query

--- api/src/main/kotlin/api/database/database.kt
+++ api/src/main/kotlin/api/database/database.kt
@@ -1,10 +1,12 @@
+import com.github.jasync.sql.db.QueryResult
 import com.github.jasync.sql.db.postgresql.PostgreSQLConnectionBuilder
+import java.util.concurrent.CompletableFuture
 
 // "jdbc:postgresql://$host:$port/$database?user=$username&password=$password"
 class Database(databasePath: String) {
     private var connection = PostgreSQLConnectionBuilder.createConnectionPool(databasePath)
 
-    fun execQuery(query: String) {
-        connection.sendPreparedStatement(query)
+    fun execQuery(query: String): CompletableFuture<QueryResult> {
+        return connection.sendPreparedStatement(query)
     }
 }

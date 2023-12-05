ae6b602bcb2b07fcc9b4f30ecc5750357bded1f7
roy-corentin <corentin.roy@epitech.eu>
2023-12-05 18:38:15 +0100

    [UP] userCreateWebHandler now get correctly body params

--- api/src/main/kotlin/api/model/user.kt
+++ api/src/main/kotlin/api/model/user.kt
@@ -1,13 +1,14 @@
 
 
 import java.time.OffsetDateTime
-class User(
+
+data class User(
     val id: Int,
     val name: String,
     val email: String,
     val created_at: OffsetDateTime,
-    val updated_at: OffsetDateTime)
-{
+    val updated_at: OffsetDateTime,
+) {
     override fun toString(): String {
         return "User(id=$id, name=$name, email=$email, created_at=$created_at, updated_at=$updated_at)"
     }
--- api/src/main/kotlin/api/webhandler/userCreateWebHandler.kt
+++ api/src/main/kotlin/api/webhandler/userCreateWebHandler.kt
@@ -1,14 +1,18 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.http.Context
 
+data class UserForm(val email: String, val name: String)
+
 class UserCreateWebHandler(val userRepository: UserRepository) : WebHandler() {
-
     override fun call(ctx: Context) {
-        val userData = ctx.bodyAsClass(User::class.java)
+        val objectMapper = jacksonObjectMapper()
+        val userObj = objectMapper.readTree(ctx.body()).get("user")
+        val userData = objectMapper.readValue(userObj.toString(), UserForm::class.java)
 
-        val name = userData?.name
-        val email = userData?.email
+        val name = userData.name
+        val email = userData.email
 
-        if (name.isNotEmpty() && email.isNotEmpty()) {
+        if (name.isNullOrEmpty() || email.isNullOrEmpty()) {
             val user = userRepository.create(name, email)
 
             if (user != null) {

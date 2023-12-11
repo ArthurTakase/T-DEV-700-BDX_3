ec2da8793e657882640b380f92bd6fbf8edceb3b
roy-corentin <corentin.roy@epitech.eu>
2023-12-11 11:29:12 +0100

    [UP] create user return the user created

--- api/src/main/kotlin/api/repositories/UserRepository.kt
+++ api/src/main/kotlin/api/repositories/UserRepository.kt
@@ -1,5 +1,4 @@
 
-import com.github.jasync.sql.db.general.ArrayRowData
 import java.time.OffsetDateTime
 
 class UserRepository(database: Database) {
@@ -7,35 +6,45 @@ class UserRepository(database: Database) {
 
     init {}
 
-    fun all() : List<User> {
+    fun all(): List<User> {
         val result = database.execQuery("select * from USERS").get()
-        val users = result.rows.map { row ->
-             UserParser(
-                id = row.get("id") as Int,
-                name = row.get("name") as String,
-                email = row.get("email") as String,
-                created_at = row.get("created_at") as OffsetDateTime,
-                updated_at = row.get("updated_at") as OffsetDateTime
-            ).parse()
-        }
+        val users =
+            result.rows.map { row ->
+                UserParser(
+                    id = row.get("id") as Int,
+                    name = row.get("name") as String,
+                    email = row.get("email") as String,
+                    created_at = row.get("created_at") as OffsetDateTime,
+                    updated_at = row.get("updated_at") as OffsetDateTime,
+                ).parse()
+            }
         println(users)
         return users
     }
 
-    fun create(name: String, email: String): User? {
-        val query = "INSERT INTO users (name, email) VALUES (?, ?)"
+    fun create(
+        name: String,
+        email: String,
+    ): User? {
+        val query = "INSERT INTO users (name, email) VALUES (?, ?) RETURNING *"
         val args = listOf(name, email)
 
         val result = database.execQuery(query, args).get()
-        val user = result.rows.map { row ->
-            UserParser(
-                id = row.get("id") as Int,
-                name = row.get("name") as String,
-                email = row.get("email") as String,
-                created_at = row.get("created_at") as OffsetDateTime,
-                updated_at = row.get("updated_at") as OffsetDateTime
-            ).parse()
-        }.firstOrNull()
+
+        if (result.rowsAffected.equals(0)) {
+            return null
+        }
+
+        val user =
+            result.rows.map { row ->
+                UserParser(
+                    id = row.get("id") as Int,
+                    name = row.get("name") as String,
+                    email = row.get("email") as String,
+                    created_at = row.get("created_at") as OffsetDateTime,
+                    updated_at = row.get("updated_at") as OffsetDateTime,
+                ).parse()
+            }.firstOrNull()
         return user
     }
 

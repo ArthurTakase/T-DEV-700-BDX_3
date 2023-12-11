bb1d4d18932d71fadc77ca5363b74c346fa6e3d4
roy-corentin <corentin.roy@epitech.eu>
2023-12-11 17:27:37 +0100

    [UP] use purchase with card number

--- api/src/main/kotlin/api/repositories/accountRepository.kt
+++ api/src/main/kotlin/api/repositories/accountRepository.kt
@@ -9,6 +9,12 @@ class AccountRepository(database: Database) {
         return account
     }
 
+    fun getByNumber(number: String): Account? {
+        val result = database.execQuery("SELECT * FROM ACCOUNTS WHERE number=$number").get()
+        val account = result.rows.map { row -> AccountParser(row).parse() }.firstOrNull()
+        return account
+    }
+
     fun purchase(
         id: Int,
         amount: Double,
@@ -17,12 +23,31 @@ class AccountRepository(database: Database) {
         update(id, (sold - amount))
     }
 
+    fun purchaseByNumber(
+        number: String,
+        amount: Double,
+    ) {
+        val sold = database.execQuery("SELECT sold FROM ACCOUNTS WHERE number=$number").get().rows[0].get("sold") as Double
+        updateByNumber(number, (sold - amount))
+    }
+
     fun update(
         id: Int,
         newSold: Double,
     ): Account {
         val result =
             database.execQuery("UPDATE ACCOUNTS SET sold=$newSold WHERE id=$id RETURNING *")
+                .get()
+        val account = result.rows.map { row -> AccountParser(row).parse() }.first()
+        return account
+    }
+
+    fun updateByNumber(
+        number: String,
+        newSold: Double,
+    ): Account {
+        val result =
+            database.execQuery("UPDATE ACCOUNTS SET sold=$newSold WHERE number=$number RETURNING *")
                 .get()
         val account = result.rows.map { row -> AccountParser(row).parse() }.first()
         return account
--- api/src/main/kotlin/api/webhandler/accountHasMoneyWebHandler.kt
+++ api/src/main/kotlin/api/webhandler/accountHasMoneyWebHandler.kt
@@ -3,16 +3,16 @@ import io.javalin.http.Context
 
 class AccountHasMoneyWebHandler(val accountRepository: AccountRepository) : WebHandler() {
     override fun call(ctx: Context) {
-        val id: Int = ctx.pathParam("id").toInt()
+        val number: String = ctx.pathParam("number")
         val objectMapper = jacksonObjectMapper()
         val amount: Double = objectMapper.readTree(ctx.body()).get("amount").toString().toDouble()
-        val account = accountRepository.get(id)
+        val account = accountRepository.getByNumber(number)
 
         if (account == null) {
             ctx.status(404).json("Account not found")
         } else {
             if (amount < account.sold) {
-                accountRepository.purchase(id, amount)
+                accountRepository.purchaseByNumber(number, amount)
                 ctx.status(200).json("ok")
             } else {
                 ctx.status(406)
